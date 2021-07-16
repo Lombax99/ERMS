e5a9d348dd9076b26db0a78eec04d459f4afde1b
Lombax99 <lombax99.ll@gmail.com>
2021-07-16 16:40:02 +0200

    model final

--- EcoRegaz/src/model/strumenti/pinza/Pinza.java
+++ EcoRegaz/src/model/strumenti/pinza/Pinza.java
@@ -9,9 +9,28 @@ public class Pinza implements IStrumento{
 	private Appartenenza appartenenza;
 	private Condizione condizione;
 
+	public Pinza(int id_Deposito, int id_Pinza, Appartenenza appartenenza, Condizione condizione) {
+		this.id_Deposito = id_Deposito;
+		this.id_Pinza = id_Pinza;
+		this.appartenenza = appartenenza;
+		this.condizione = condizione;
+	}
+
 	@Override
 	public int getId_Deposito() {
 		return 0;
+	}
+
+	public int getId_Pinza() {
+		return id_Pinza;
+	}
+
+	public Appartenenza getAppartenenza() {
+		return appartenenza;
+	}
+
+	public Condizione getCondizione() {
+		return condizione;
 	}
 
 }
--- EcoRegaz/src/model/strumenti/scatolaGuanti/ScatolaGuanti.java
+++ EcoRegaz/src/model/strumenti/scatolaGuanti/ScatolaGuanti.java
@@ -1,18 +1,20 @@
 package model.strumenti.scatolaGuanti;
 
-public class ScatolaGuanti {
+import model.strumenti.IStrumento;
+
+public class ScatolaGuanti implements IStrumento{
 
 	private int id_Deposito;
 	private Taglia taglia;
 	private int id_ScatolaGuanti;
 	
 	public ScatolaGuanti(int id_Deposito, Taglia taglia, int id_ScatolaGuanti) {
-		super();
 		this.id_Deposito = id_Deposito;
 		this.taglia = taglia;
 		this.id_ScatolaGuanti = id_ScatolaGuanti;
 	}
 
+	@Override
 	public int getId_Deposito() {
 		return id_Deposito;
 	}
@@ -25,5 +27,4 @@ public class ScatolaGuanti {
 		return id_ScatolaGuanti;
 	}
 
-	
 }
