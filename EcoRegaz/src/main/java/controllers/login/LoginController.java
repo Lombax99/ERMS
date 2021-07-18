c445837830826dba0c77e872a8552dc86ea10b47
RiccardoEvangelisti <evangericcardo00@gmail.com>
2021-07-18 13:31:06 +0200

    Implementazione PersisterLog

--- EcoRegaz/src/main/java/controllers/login/LoginController.java
+++ EcoRegaz/src/main/java/controllers/login/LoginController.java
@@ -4,34 +4,40 @@ import java.time.LocalDateTime;
 
 import main.java.persisters.logPersister.PersisterLog;
 
+/**
+ * Classe che si occupa di verificare la password e delegare a PersisterLog la scrittura del log.
+ *
+ */
 public class LoginController {
 
 	private PersisterLog persisterReference;
-	/* Stringa fake value per il funzionamento del prototipo
-	 * Sostituisce il file in cui verrà memorizzata la password reale*/
 	
-	private final String password="SuperUserPassword";
-	
+	/* 
+	 * Stringa fake value per il funzionamento del prototipo
+	 * Sostituisce il file in cui verrà memorizzata la password reale
+	 * TODO
+	 */
+	private static final String password = "SuperUserPassword";
+
+	/*
+	 * Costruttore
+	 */
 	public LoginController() {
-		persisterReference = new PersisterLog(); 
+		persisterReference = new PersisterLog();
 	}
-	
-	
-	/* Controlla il contenuto della stringa
+
+	/* 
+	 * Controlla il contenuto della stringa
 	 * NB: Da cambiare quando verrà utilizzato un file crittato 
-	 * 	   contentente la password reale*/
-	public boolean verificaCredenziali(String text)
-	{
+	 * 	   contentente la password reale
+	 */
+	public boolean verificaCredenziali(String text) {
 		boolean value = text.contentEquals(password);
 		this.scritturaEntry(value);
 		return value;
 	}
 
-	
-	public void scritturaEntry(boolean correttezza)
-	{
+	public void scritturaEntry(boolean correttezza) {
 		persisterReference.scritturaEntry(LocalDateTime.now(), correttezza);
 	}
 }
-
-
--- EcoRegaz/src/main/java/persisters/logPersister/PersisterLog.java
+++ EcoRegaz/src/main/java/persisters/logPersister/PersisterLog.java
@@ -1,12 +1,50 @@
 package main.java.persisters.logPersister;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDateTime;
 
+import main.java.application.AlertPanel;
+
+/**
+ * Classe che si occupa della scrittura del log su file apposito
+ */
 public class PersisterLog {
 
-	public void scritturaEntry(LocalDateTime now, boolean correttezza) {
-		// TODO Auto-generated method stub
-		
+	/**
+	 * Istanza di File che identifica il logFile, salvato allo stesso livello del programma
+	 */
+	private static final File logFile = new File("./log");
+
+	/**
+	 * Metodo che scrive la entry nel formato: LocalDateTime successo/fallimento <br>
+	 * sul file di log posizionato in "./log"
+	 */
+	public void scritturaEntry(LocalDateTime dataOra, boolean correttezza) {
+
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new FileWriter(logFile));
+
+			String entry = dataOra.toString() + "\t";
+			if (correttezza) {
+				entry.concat("successo");
+			} else {
+				entry.concat("fallimento");
+			}
+
+			pw.append(entry);
+
+		} catch (IOException e) {
+
+			AlertPanel.saysError("ERRORE: nella scrittura del log", e);
+
+		} finally {
+			pw.close();
+		}
+
 	}
 
 }
