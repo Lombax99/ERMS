5fbe60c9032559e1849461cab4d8b620a03a363c
evang <evang@RiccardoEvangelisti-PC.homenet.telecomitalia.it>
2021-07-20 22:53:07 +0200

    aggiunto costruttore

--- EcoRegaz/src/main/java/views/interfacciaInterventi/HomeInterventi.java
+++ EcoRegaz/src/main/java/views/interfacciaInterventi/HomeInterventi.java
@@ -1,5 +1,11 @@
 package main.java.views.interfacciaInterventi;
 
+import javafx.stage.Stage;
+
 public class HomeInterventi {
+
+	public HomeInterventi(Stage primaryStage) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
