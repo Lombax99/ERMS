2f245749da0bd04db7a046706098b1ef32dc6f48
RiccardoEvangelisti <evangericcardo00@gmail.com>
2021-07-20 16:20:42 +0200

    Aggiunta funzione da String a Enum

--- EcoRegaz/src/main/java/models/areaVerde/Quartiere.java
+++ EcoRegaz/src/main/java/models/areaVerde/Quartiere.java
@@ -1,33 +1,57 @@
 package main.java.models.areaVerde;
 
 public enum Quartiere {
-	BORGOPANIGALE_RENO,
-	PORTO_SARAGOZZA,
-	NAVILE,
-	SANDONATO_SANVITALE,
-	SAVENA,
-	SANTOSTEFANO;
+	BORGOPANIGALE_RENO, PORTO_SARAGOZZA, NAVILE, SANDONATO_SANVITALE, SAVENA, SANTOSTEFANO;
 
-
-
-@Override
-public String toString() {
-	switch (this) 
-	{
-	case BORGOPANIGALE_RENO: return "Borgo Panigale - Reno";
-	
-	case PORTO_SARAGOZZA: return "Porto - Saragozza";
-
-	case NAVILE: return "Navile";
-
-	case SANDONATO_SANVITALE: return "San Donato - San Vitale";
-
-	case SAVENA: return "Savena";
-
-	case SANTOSTEFANO: return "Santo Stefano";
-	
-	default: throw new IllegalArgumentException();
+	/**
+	 * Metodo che da stringa restituisce l'enum corretto.
+	 */
+	public static Quartiere toEnum(String quartiere) {
+		switch (quartiere) {
+		case "Borgo Panigale - Reno":
+			return BORGOPANIGALE_RENO;
+		case "Porto - Saragozza":
+			return PORTO_SARAGOZZA;
+		case "Navile":
+			return NAVILE;
+		case "San Donato - San Vitale":
+			return SANDONATO_SANVITALE;
+		case "Savena":
+			return SAVENA;
+		case "Santo Stefano":
+			return SANTOSTEFANO;
+		default:
+			throw new IllegalArgumentException();
+		}
 	}
-}
+
+	/**
+	 * Metodo adattato che restituisce la corrispondente forma corretta.
+	 */
+	@Override
+	public String toString() {
+		switch (this) {
+		case BORGOPANIGALE_RENO:
+			return "Borgo Panigale - Reno";
+
+		case PORTO_SARAGOZZA:
+			return "Porto - Saragozza";
+
+		case NAVILE:
+			return "Navile";
+
+		case SANDONATO_SANVITALE:
+			return "San Donato - San Vitale";
+
+		case SAVENA:
+			return "Savena";
+
+		case SANTOSTEFANO:
+			return "Santo Stefano";
+
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
 
 }
