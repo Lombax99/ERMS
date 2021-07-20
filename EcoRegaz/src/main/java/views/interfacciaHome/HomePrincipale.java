1621b6d08740ca84627e76bd34f55c1b3a7588d2
evang <evang@RiccardoEvangelisti-PC.homenet.telecomitalia.it>
2021-07-20 22:09:45 +0200

    Home Principale: devo riavviare il pc

--- EcoRegaz/src/main/java/views/interfacciaHome/HomePrincipale.java
+++ EcoRegaz/src/main/java/views/interfacciaHome/HomePrincipale.java
@@ -1,5 +1,45 @@
 package main.java.views.interfacciaHome;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class HomePrincipale {
+	
+	public final static String homePrincipaleURL = "/main/java/views/interfacciaHome/HomePrincipale.fxml";
+	
+	@FXML
+	private void openInterventiHandler(ActionEvent event) {
+		
+	}
+	
+	
+	@FXML
+	private void openRendicontazioneHandler(ActionEvent event) {
+		
+	}
+	
+	
+	@FXML
+	private void openAnalisiAreeVerdiHandler(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	private void openStrumentiHandler(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	private void openDepositiHandler(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	private void openIscrittiHandler(ActionEvent event) {
+		
+	}
+	
+	
+	
 
 }
