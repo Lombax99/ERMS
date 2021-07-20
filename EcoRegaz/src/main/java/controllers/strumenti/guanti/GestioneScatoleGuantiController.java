0df2e655ff9a69214bbd256b588a86103a8f3648
RiccardoEvangelisti <evangericcardo00@gmail.com>
2021-07-20 13:55:44 +0200

    da fare

--- EcoRegaz/src/main/java/controllers/strumenti/guanti/GestioneScatoleGuantiController.java
+++ EcoRegaz/src/main/java/controllers/strumenti/guanti/GestioneScatoleGuantiController.java
@@ -5,50 +5,44 @@ import java.sql.SQLException;
 
 import main.java.application.AlertPanel;
 import main.java.controllers.strumenti.IGestioneStrumentiController;
+import main.java.models.strumenti.IStrumento;
 import main.java.models.strumenti.scatolaGuanti.ScatolaGuanti;
-import main.java.persisters.strumenti.deposito.PersisterDeposito;
+import main.java.persisters.strumenti.scatoleGuanti.PersisterScatoleGuanti;
 
-public class GestioneScatoleGuantiController  implements IGestioneStrumentiController{
+public class GestioneScatoleGuantiController implements IGestioneStrumentiController {
 
-			
-	public boolean aggiuntaNuovoStrumento(ScatolaGuanti strumento) {
-		
-		
-		
-		try{
-			if (PersisterScatoleGuanti.getInstance().aggiuntaNuovaScatola(strumento))
-			return true;
-		} catch(SQLException e) {
+	@Override
+	public boolean aggiuntaNuovoStrumento(IStrumento strumento) {
+
+		try {
+			if (PersisterScatoleGuanti.getInstance().aggiuntaNuovaScatola((ScatolaGuanti) strumento))
+				return true;
+		} catch (SQLException e) {
 			AlertPanel.saysError("Errore nell'inserimento dello strumento nel database", e);
 		}
-	return false;
-}
+		return false;
+	}
 
-	
+	@Override
 	public boolean rimozioneStrumento(int ID_ScatolaGuanti) {
-		try{
+		try {
 			if (PersisterScatoleGuanti.getInstance().rimozioneScatola(ID_ScatolaGuanti))
-			return true;
-		} catch(SQLException e) {
+				return true;
+		} catch (SQLException e) {
 			AlertPanel.saysError("Errore nella rimozione dello strumento nel database", e);
 		}
-	return false;
-}
-
-	
-	public ResultSet elencoStrumenti() {
-		
+		return false;
 	}
-	
-	public boolean modificaDatiStrumento(ScatolaGuanti strumento) {
-		try{
-			if (PersisterScatoleGuanti.getInstance().modificaDatiScatola(strumento))
-			return true;
-		} catch(SQLException e) {
-			AlertPanel.saysError("Errore nella modifica dello strumento nel database", e);
-		}
-	return false;
-}
-		
-}
 
+	@Override
+	public ResultSet elencoStrumenti() {
+		return null;
+	}
+
+	@Override
+	public boolean modificaDatiStrumento(IStrumento strumento) {
+		// non deve essere implementata 
+		return false;
+	}
+
+}
