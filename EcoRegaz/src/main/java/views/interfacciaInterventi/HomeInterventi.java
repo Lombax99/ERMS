f90b7379cf047ce2ff12f43644f6a5221b41caeb
RiccardoEvangelisti <evangericcardo00@gmail.com>
2021-07-21 13:48:32 +0200

    HomeInterventi: iniziata implementazione

--- EcoRegaz/src/main/java/views/interfacciaInterventi/HomeInterventi.java
+++ EcoRegaz/src/main/java/views/interfacciaInterventi/HomeInterventi.java
@@ -1,11 +1,39 @@
 package main.java.views.interfacciaInterventi;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import main.java.views.Utility_SidePanel;
+
+import com.jfoenix.controls.JFXDrawer;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class HomeInterventi implements Initializable {
 
 
-public class HomeInterventi {
 
-	public HomeInterventi() {
-		// TODO Auto-generated constructor stub
+
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+		/*
+		 * Set up del sidePanel
+		 */
+		Utility_SidePanel.initialize(exitButton, sidePanel);
+
 	}
+	
+	
+	/*
+	 * Definizione dei componenti grafici
+	 */
+	@FXML
+	private JFXDrawer sidePanel;
+	@FXML
+	private Button exitButton;
 
 }
