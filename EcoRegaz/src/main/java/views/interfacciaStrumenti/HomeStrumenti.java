30bf3dd2eef9a0d1cf7804ce39964168e0ad336c
RiccardoEvangelisti <evangericcardo00@gmail.com>
2021-07-22 00:50:18 +0200

    implementata

--- EcoRegaz/src/main/java/views/interfacciaStrumenti/HomeStrumenti.java
+++ EcoRegaz/src/main/java/views/interfacciaStrumenti/HomeStrumenti.java
@@ -1,9 +1,37 @@
 package main.java.views.interfacciaStrumenti;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import main.java.views.GestoreInterfacce;
+
 public class HomeStrumenti {
 
-	public HomeStrumenti() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * Metodo handler del bottone Guanti. <br>
+	 */
+	@FXML
+	void openGuantiHandler(ActionEvent event) {
+		GestoreInterfacce.getInstance().initViewGuanti();
+	}
+
+
+	/**
+	 * Metodo handler del bottone Pinze. <br>
+	 */
+	@FXML
+	void openPinzeHandler(ActionEvent event) {
+		GestoreInterfacce.getInstance().initViewPinze();
+	}
+
+
+	/**
+	 * Metodo handler del bottone Sacchi. <br>
+	 */
+	@FXML
+	void openSacchiHandler(ActionEvent event) {
+
+		GestoreInterfacce.getInstance().initViewSacchi();
+
 	}
 
 }
