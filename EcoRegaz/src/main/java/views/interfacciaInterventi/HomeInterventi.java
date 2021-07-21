e3f96e2ece430fec372aa0cd3c94955c58413dbc
RiccardoEvangelisti <evangericcardo00@gmail.com>
2021-07-21 14:06:02 +0200

    funziona

--- EcoRegaz/src/main/java/views/interfacciaInterventi/HomeInterventi.java
+++ EcoRegaz/src/main/java/views/interfacciaInterventi/HomeInterventi.java
@@ -3,17 +3,43 @@ package main.java.views.interfacciaInterventi;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import main.java.views.GestoreInterfacce;
 import main.java.views.Utility_SidePanel;
 
 import com.jfoenix.controls.JFXDrawer;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
+/**
+ * Classe che si limita a ridirezionare a GestioneInterfacce l'apertura delle View di Aggiunta e Visualizzazione Interventi
+ *
+ */
 public class HomeInterventi implements Initializable {
 
 
+	/**
+	 * Metodo handler del bottone Aggiungi Intervento. <br>
+	 */
+	@FXML
+	private void openAggiungiInterventoHandler(ActionEvent event) {
 
+		GestoreInterfacce.getInstance().initViewAggiuntaIntervento();
+
+	}
+
+
+	/**
+	 * Metodo handler del bottone Vedi Interventi. <br>
+	 */
+	@FXML
+	private void openVediInterventiHandler(ActionEvent event) {
+
+		GestoreInterfacce.getInstance().initViewVediInterventi();
+
+	}
 
 
 
@@ -26,8 +52,8 @@ public class HomeInterventi implements Initializable {
 		Utility_SidePanel.initialize(exitButton, sidePanel);
 
 	}
-	
-	
+
+
 	/*
 	 * Definizione dei componenti grafici
 	 */
