b4e8d0541856f4f2dbf10813080ceda7b2ba9ec9
Lombax99 <lombax99.ll@gmail.com>
2021-07-22 16:51:33 +0200

    ViewPinze WIP

--- EcoRegaz/src/main/java/views/interfacciaStrumenti/interfacciaPinze/ViewPinze.java
+++ EcoRegaz/src/main/java/views/interfacciaStrumenti/interfacciaPinze/ViewPinze.java
@@ -2,11 +2,17 @@ package main.java.views.interfacciaStrumenti.interfacciaPinze;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
-
+import java.util.StringTokenizer;
 
 import com.jfoenix.controls.JFXDrawer;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -15,13 +21,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import main.java.application.AlertPanel;
+import main.java.controllers.strumenti.pinze.FiltroPinze;
 import main.java.controllers.strumenti.pinze.GestionePinzeController;
+import main.java.models.iscritto.Iscritto;
 import main.java.models.strumenti.pinza.Appartenenza;
 import main.java.models.strumenti.pinza.Condizione;
+import main.java.models.strumenti.pinza.Pinza;
+import main.java.persisters.iscritti.PersisterIscritti;
+import main.java.persisters.strumenti.pinze.PersisterPinze;
 import main.java.views.Utility_SidePanel;
 import main.java.views.interfacciaLogin.HomeLogin;
 
@@ -43,7 +58,17 @@ public class ViewPinze implements Initializable {
 	private ListView<String> listViewDepositi;
     @FXML
     private Button fineButton;
-
+	@FXML
+	private TableView<ObservableList<String>> tabella;
+    @FXML
+	private TableColumn<ObservableList<String>, String> Id_PinzaColumn;
+	@FXML
+	private TableColumn<ObservableList<String>, String> CondizioneColumn;
+	@FXML
+	private TableColumn<ObservableList<String>, String> AppartenenzaColumn;
+	@FXML
+	private TableColumn<ObservableList<String>, String> Id_DepColumn;
+	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -54,20 +79,78 @@ public class ViewPinze implements Initializable {
 		Utility_SidePanel.initialize(exitButton, sidePanel);
 
 		controllerPinze = new GestionePinzeController();
-
-
+		
+		// TODO da rimuovere
+		try {
+			PersisterPinze.getInstance().aggiuntaNuovaPinza(new Pinza(0, 0, Appartenenza.PROPRIA, Condizione.FUNZIONANTE));
+			PersisterPinze.getInstance().aggiuntaNuovaPinza(new Pinza(0, 0, Appartenenza.PRESTATA, Condizione.FUNZIONANTE));
+			PersisterPinze.getInstance().aggiuntaNuovaPinza(new Pinza(0, 0, Appartenenza.PROPRIA, Condizione.NONFUNZIONANTE));
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		} // fin qui
 
 		/*
-		 * Prelevamento depositi
+		 * setUp tabella grafica
 		 */
-		// ResultSet result = gp.elencoStrumenti(new FiltroPinze());
+		//prendo tutte le pinze con un filtro vuoto
+		ResultSet ElencoPinze = controllerPinze.elencoStrumenti(new FiltroPinze());
 
-		// List<String> listaDepositi = new ArrayList<>();
-		/*while(result.next()) {
-			listaDepositi.add(result.getString(4));
+
+		//setUp delle colonne
+		Id_PinzaColumn.setCellValueFactory(new Callback<CellDataFeatures<ObservableList<String>, String>, ObservableValue<String>>() {
+
+			public ObservableValue<String> call(CellDataFeatures<ObservableList<String>, String> param) {
+				// param.getValue() restituisce l'istanza ObservableList<String> di una particolare riga,
+				// .get(1) prende il primo parametro della riga
+				return new SimpleStringProperty(param.getValue().get(0).toString());
+			}
+		});
+
+		CondizioneColumn.setCellValueFactory(new Callback<CellDataFeatures<ObservableList<String>, String>, ObservableValue<String>>() {
+
+			public ObservableValue<String> call(CellDataFeatures<ObservableList<String>, String> param) {
+				return new SimpleStringProperty(param.getValue().get(3).toString());
+			}
+		});
+
+		AppartenenzaColumn.setCellValueFactory(new Callback<CellDataFeatures<ObservableList<String>, String>, ObservableValue<String>>() {
+
+			public ObservableValue<String> call(CellDataFeatures<ObservableList<String>, String> param) {
+				return new SimpleStringProperty(param.getValue().get(2).toString());
+			}
+		});
+
+		Id_DepColumn.setCellValueFactory(new Callback<CellDataFeatures<ObservableList<String>, String>, ObservableValue<String>>() {
+
+			public ObservableValue<String> call(CellDataFeatures<ObservableList<String>, String> param) {
+				return new SimpleStringProperty(param.getValue().get(1).toString());
+			}
+		});
+
+
+		// popolo la tabella
+		ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
+
+		// Iterate Row
+		try {
+			while (ElencoPinze.next()) {
+
+				ObservableList<String> row = FXCollections.observableArrayList();
+
+				// Iterate Column
+				for (int i = 1; i <= ElencoPinze.getMetaData().getColumnCount(); i++) {
+					
+					row.add(ElencoPinze.getString(i));
+				}
+				data.add(row);
+			}
+
+		} catch (SQLException e) {
+			AlertPanel.saysError("ERRORE nella popolazione della tabella", e);
 		}
-		*/
-		// listViewDepositi.setItems(FXCollections.observableArrayList(listaDepositi));
+
+		tabella.setItems(data);
+		
 	}
 
 
@@ -93,25 +176,5 @@ public class ViewPinze implements Initializable {
 		inserisciDatiPinzaStage.setScene(popUpScene);
 		inserisciDatiPinzaStage.initModality(Modality.APPLICATION_MODAL);
 		inserisciDatiPinzaStage.show();
-		
-		/*
-		 * Popolamento della lista 
-		 */
-		/*List<AreaVerde> listaAreeVerdi = visualizzazioneController.visualizza(areaVerdeCerca.getText());
-		List<String> listaNomi = new ArrayList<>();
-		
-		for (AreaVerde areaVerde : listaAreeVerdi) {
-			listaNomi.add(areaVerde.getNome() + "\n" + areaVerde.getQuartiere());
-		}
-		
-		ListView<String> listView = new ListView<>(FXCollections.observableArrayList(listaNomi));
-		
-		/*
-		 * Lo stage si chiude quando viene cliccato un elemento della listview.
-		 * Inoltre vengono riempiti i field nella ViewAggiuntaIntervento.
-		 */
-		/*listView.getSelectionModel().selectedItemProperty().addListener((ObservableValue<? extends String> ov, String old_val, String new_val) -> {
-		
-			selezionaAreaVerdeStage.close();*/
 	}
 }
